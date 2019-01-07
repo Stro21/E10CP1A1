def8950dd9ae1e0833e964d62c202fee10cd70d1
Eduardo Straub <eduardo.straub1@gmail.com>
2019-01-06 23:08:15 -0300

    Funciona Test con Student.

--- ejercicio6.rb
+++ ejercicio6.rb
@@ -1,7 +1,7 @@
 # This is the module of a test.
 module Test
   def result(nota1, nota2)
-    if (nota1 + nota2) / 2 >= 4
+    if ((nota1 + nota2) / 2.0).round(1) >= 4
       puts 'Estudiante aprobado'
     else
       puts 'Estudiante reprobado'
@@ -17,9 +17,13 @@ end
 class Student
   include Test
   include Attendance
+  attr_reader :grade1, :grade2
   def initialize(nombre, nota1 = 4, nota2 = 4)
     @name = nombre
     @grade1 = nota1
     @grade2 = nota2
   end
 end
+
+student = Student.new('Eduardo', 6.5, 7)
+student.result(student.grade1, student.grade2)
