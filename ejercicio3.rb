abf595baf90fdbfc907b6f760a6e8310630bbc30
Eduardo Straub <eduardo.straub1@gmail.com>
2019-01-06 21:08:19 -0300

    Se completo el ejercicio 3.

--- ejercicio3.rb
+++ ejercicio3.rb
@@ -1,3 +1,5 @@
+# rubocop:disable ClassVars
+
 # This is the class of a Vehicle
 class Vehicle
   attr_accessor :model, :year, :start
@@ -12,6 +14,29 @@ class Vehicle
   end
 end
 
+# This is inherence class of Vehicle.
 class Car < Vehicle
+  @@intances = 0
+  def initialize(model, year)
+    @model = model
+    @year = year
+    @start = false
+    @@intances += 1
+  end
 
+  def engine_start
+    @start = true
+    puts 'El motor se ha encendido!'
+  end
+
+  def self.intances_counter
+    @@intances
+  end
 end
+
+10.times do
+  Car.new('Sedan', 2014)
+end
+puts Car.intances_counter
+
+# rubocop:enable ClassVars
