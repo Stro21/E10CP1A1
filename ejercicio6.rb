03dfa087fdd68768340d91597d66f5c8e675165d
Eduardo Straub <eduardo.straub1@gmail.com>
2019-01-06 22:22:15 -0300

    Se avanza en el ejercicio 6.

--- ejercicio6.rb
+++ ejercicio6.rb
@@ -1,5 +1,12 @@
 # This is the module of a test.
 module Test
+  def result(nota1, nota2)
+    if (nota1 + nota2) / 2 >= 4
+      puts 'Estudiante aprobado'
+    else
+      puts 'Estudiante reprobado'
+    end
+  end
 end
 
 # This is the module of Attendance.
@@ -8,4 +15,11 @@ end
 
 # This is a class of a Student.
 class Student
+  include Test
+  include Attendance
+  def initialize(nombre, nota1 = 4, nota2 = 4)
+    @name = nombre
+    @grade1 = nota1
+    @grade2 = nota2
+  end
 end
